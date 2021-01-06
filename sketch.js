2f41e87a84bc0c6d7b7351d1187a093fa5bb2dd3
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2021-01-06 12:16:09 +0100

    added board

--- sketch.js
+++ sketch.js
@@ -1,31 +1,48 @@
+var squares = [];
+var pieces = [];
+
+var cnv;
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
 function setup() {
-  createCanvas(500, 800);
+  cnv = createCanvas(500, 800);
+  centerCanvas();
+  background(200);
   drawBoard();
 }
 
-function drawBoard() {
-  black = 220;
-  white = 30;
-  for (y = 0; y < height; y += 50) {
-    for (x = 0; x < width; x += 50) {
-      if (x % 100 == 0) {
-        if (y % 100 == 0) {
-          fill(black);
-        }
-        if (y % 100 == 50) {
-          fill(white);
-        }
-      }
-      if (x % 100 == 50) {
-        if (y % 100 == 50) {
-          fill(black);
-        }
-        if (y % 100 == 0) {
-          fill(white);
-        }
-      }
+function windowResized() {
+  centerCanvas();
+}
 
-      rect(x, y, 50, 50);
+function drawBoard() {
+  let rows = 0
+  let firstColor = 'black'
+  let secondColor = 'white'
+
+  while (rows < 16) {
+    if (rows % 2 == 0) {
+      firstColor = 'white'
+      secondColor = 'black'
+    } else {
+      firstColor = 'black'
+      secondColor = 'white'
     }
+    let cols = 0
+    while (cols < 10) {
+      if (cols % 2 == 0) {
+        fill(firstColor)
+      } else {
+        fill(secondColor)
+      }
+      rect(cols * 50, rows * 50, 50, 50)
+      cols += 1
+    }
+    rows += 1
   }
 }
