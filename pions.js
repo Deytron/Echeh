d008664fd9f1705a10d9e7af19d11e7c0ab2dc91
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2021-01-18 11:48:13 +0100

    Basic piecces

--- pions.js
+++ pions.js
@@ -1,3 +1,21 @@
-export function pions() {
-  line(1, 1, 1);
+class King {
+  constructor(square, color) {
+    this.x = square.x + 25;
+    this.y = square.y + 25;
+    this.color = color;
+    this.height = 50;
+    this.width = 50;
+    this.pv = 50;
+    this.attaque = 30;
+    this.moveCost = 1;
+    this.chanceCrit = Math.round(random(1, 20));
+  }
+
+  show() {
+    if (color == 0) {
+      image(kingBlackImg, 25, 25);
+    } else {
+      image(kingWhiteImg, x, y);
+    }
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+//Définition tableau
 var squares = [];
-var pieces = [];
+var pions = [];
 
+//Définition valeurs jeu
 var cnv;
+var tour;
+var blanc = 255;
+var noir = 0;
+let font,
+  fontsize = 36;
+var deplacements = 20;
+var totalSeconds = 0;
+var minu = 0;
+var sec = 0;
+
+//Chargement des assets avant que les fonctions draw() soient appelées
+function preload() {
+  font = loadFont("assets/opensans.ttf");
+  kingWhiteImg = loadImage("assets/king.png");
+  kingBlackImg = loadImage("assets/king_b.png");
+}
 
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
@@ -10,39 +28,105 @@ function centerCanvas() {
 }
 
 function setup() {
-  cnv = createCanvas(500, 800);
+  cnv = createCanvas(700, 800);
   centerCanvas();
-  background(200);
-  drawBoard();
+  //Creation squares
+  for (y = 0; y < height; y += 50) {
+    for (x = 0; x < width; x += 50) {
+      square = new Square(x, y);
+      squares.push(square);
+      // console.log(square);
+    }
+  }
+  //Texte
+  textFont(font);
+  textSize(fontsize);
+  textAlign(CENTER, CENTER);
+  //Sélection tour aléatoire au setup
+  tour = Math.round(random());
+  console.log(tour);
+  //Timer
+  setInterval(stopwatch, 1000);
+
+  //Push pions
+  pions.push(new King(squares[0], 1));
+}
+
+function newTour() {
+  toi = 0;
 }
 
 function windowResized() {
   centerCanvas();
 }
 
+//Draw de tout
+function draw() {
+  background(200);
+  drawBoard();
+  //Draw texte
+  drawWords();
+  //Draw pions
+  for (i = 0; i < pions.length; i++) {
+    pions[i].show();
+    // console.log(pions);
+  }
+}
+
 function drawBoard() {
-  let rows = 0
-  let firstColor = 'black'
-  let secondColor = 'white'
+  let rows = 0;
+  let firstColor = "black";
+  let secondColor = "white";
 
   while (rows < 16) {
     if (rows % 2 == 0) {
-      firstColor = 'white'
-      secondColor = 'black'
+      firstColor = "white";
+      secondColor = "black";
     } else {
-      firstColor = 'black'
-      secondColor = 'white'
+      firstColor = "black";
+      secondColor = "white";
     }
-    let cols = 0
+    let cols = 0;
     while (cols < 10) {
       if (cols % 2 == 0) {
-        fill(firstColor)
+        fill(firstColor);
       } else {
-        fill(secondColor)
+        fill(secondColor);
       }
-      rect(cols * 50, rows * 50, 50, 50)
-      cols += 1
+      rect(cols * 50, rows * 50, 50, 50);
+      cols += 1;
     }
-    rows += 1
+    rows += 1;
   }
+}
+
+function stopwatch() {
+  textsec.clear();
+  textminu.clear();
+  totalSeconds++;
+  sec = totalSeconds % 60;
+  if (sec >= 59) {
+    minu++;
+  }
+  // console.log(totalSeconds);
+  // console.log(sec);
+  // console.log(minu);
+}
+
+function drawWords() {
+  textAlign(RIGHT);
+
+  if (tour == 0) {
+    fill(blanc);
+    text("Tour des", 660, 50);
+    text("blancs", 660, 150);
+  } else {
+    fill(noir);
+    text("Tour des", 660, 50);
+    text("noirs", 660, 150);
+  }
+  fill(blanc);
+  textminu = text(minu, 575, 702);
+  text(":", 600, 700);
+  textsec = text(sec, 650, 702);
 }
